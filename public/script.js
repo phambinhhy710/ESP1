24344468ab00dac7870319c2e2c4da521a2ddbbc
Binh <phambinhhy710@.com>
2022-03-23 11:42:07 +0700

    a

---
+++ public/script.js
@@ -0,0 +1,33 @@
+var socket = io("https://esp2.herokuapp.com");
+
+socket.on("server-send-browser", function(data){
+    if (data.value == "On") {
+        if(data.address=="ledOn"){
+            $("#ledOn").css("background-color", "green");
+        } else{
+            $("#ledStart").css("background-color", "green");
+        };              
+    } else {
+        if(data.address=="ledOn"){
+            $("#ledOn").css("background-color", "red");
+        } else{
+            $("#ledStart").css("background-color", "red");
+        }
+    }    
+});
+
+$(function() {
+    $("#btnOn").on("click", function() {
+        socket.emit("browser-send-data", {name: "Browser", address: "btnOn", value: "On"});
+    });
+    $("#btnOff").on("click", function() {
+        socket.emit("browser-send-data", {name: "Browser", address: "btnOff", value: "On"});
+    });
+
+    $("#btnStart").on("click", function() {
+        socket.emit("browser-send-data", {name: "Browser", address: "btnStart", value: "On"});
+    });
+    $("#btnStop").on("click", function() {
+        socket.emit("browser-send-data", {name: "Browser", address: "btnStop", value: "On"});
+    });
+});
